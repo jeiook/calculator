904edd76587be55fd863679ce109e04f7115c0ba
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2018-10-08 18:24:22 -0700

    basic structure added
    Only the very basic structures and logic (and only some of it) were added.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-$(document).ready(function() {});
+var default_calc = "standard";
+var current_calc = default_calc;
+
+$(document).ready(function() {
+	$("body").children().css({"display": "none"});
+	//choose which calculator to display
+	function choose_calc() {
+		if (current_calc === "standard") {
+			$("#standard").css("display", "block");
+		}
+	}
+	choose_calc();
+});
